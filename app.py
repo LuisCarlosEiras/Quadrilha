bcc49dc25685fc699203712478c2fd9dc5e031d4
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2023-12-23 12:12:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,7 @@ with open("grafo.html", "r") as f:
 
 st.header("Quadrilha")
 
-# st.write('---')
 
-# st.subheader('Using st.markdown')
-st.code('''st.markdown(html_data, unsafe_allow_html=True)''', language='python')
-st.markdown(html_data, unsafe_allow_html=True)
-
-st.write('---')
-
-st.subheader('Using st.markdown with iframe')
-st.code('''st.markdown(f'<iframe srcdoc="{html_data}" width="1000" height="600"></iframe>', unsafe_allow_html=True)''', language='python')
-st.markdown(f'<iframe srcdoc="{html_data}" width="1000" height="600"></iframe>', unsafe_allow_html=True)
-
-st.write('---')
 
 st.subheader('Using components.v1.html')
 st.code('''st.components.v1.html(html_data)''', language='python')
