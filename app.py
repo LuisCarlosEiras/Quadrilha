0b86c5195b10440b4f64034bc9ba1310bd56d489
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2023-12-23 13:38:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import streamlit as st
-
+st.header("Quadrilha")
 # Read the HTML file
 with open("grafo.html", "r") as f:
     html_data = f.read()
 # Display the grafo.html using st.components.v1.html for full control
 st.components.v1.html(html_data, height=800)  # Adjust height as needed
 
-st.header("Quadrilha")
-    
+   
 # st.code('''st.markdown(f'<iframe srcdoc="{html_data}" width="1000" height="600"></iframe>', unsafe_allow_html=True)''', language='python')
 # st.markdown(f'<iframe srcdoc="{html_data}" width="1000" height="600"></iframe>', unsafe_allow_html=True)
 
