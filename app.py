88c30e244c0327feafd96b16ebdce65730f1547a
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2023-12-23 14:01:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 import streamlit as st
+
 st.header("Quadrilha, Drummond")
 st.subheader('Com o mouse sobre a seta, aparece o verbo')
 st.subheader('Apertando o botão do mouse sobre a aresta(círculo), você pode movimentar o grafo') 
+
 # Read the HTML file
 with open("grafo.html", "r") as f:
     html_data = f.read()
+
 # Display the grafo.html using st.components.v1.html for full control
 st.components.v1.html(html_data, height=800)  # Adjust height as needed
-
-   
-# st.code('''st.markdown(f'<iframe srcdoc="{html_data}" width="1000" height="600"></iframe>', unsafe_allow_html=True)''', language='python')
-# st.markdown(f'<iframe srcdoc="{html_data}" width="1000" height="600"></iframe>', unsafe_allow_html=True)
-
-# st.code('''st.components.v1.html(html_data)''', language='python')
 st.components.v1.html(html_data)
 
 
