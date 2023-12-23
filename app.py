42bf56a4404baa7ce7ec61ffa79f666daffd7e87
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2023-12-23 13:21:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,7 @@
 # https://discuss.streamlit.io/t/unable-to-load-html-file-in-streamlit-app/40186/2
 
-import streamlit as st
-import pyvis.network as Network
-import networkx as nx
+# Carrega o arquivo grafo.html
+html_file = open("grafo.html", "r").read()
 
-# Criação do grafo direcionado
-G = nx.DiGraph()
-
-# Adicionando as arestas ao grafo
-G.add_edge("João", "Teresa", action="amava")
-G.add_edge("Teresa", "Raimundo", action="amava")
-G.add_edge("Raimundo", "Maria", action="amava")
-G.add_edge("Maria", "Joaquim", action="amava")
-G.add_edge("Joaquim", "Lili", action="amava")
-G.add_edge("Lili", "ninguém", action="amava")
-G.add_edge("João", "Estados Unidos", action="foi pra")
-G.add_edge("Teresa", "convento", action="foi para")
-G.add_edge("Raimundo", "desastre", action="morreu de")
-G.add_edge("Maria", "tia", action="ficou para")
-G.add_edge("Joaquim", "suicídio", action="cometeu")
-G.add_edge("Lili", "J. Pinto Fernandes", action="casou com")
-G.add_edge("J. Pinto Fernandes", "história", action="que não tinha entrado na")
-G.add_edge("Quadrilha","Drummond", action="autor")
-
-# Criação do grafo interativo
-net = Network(notebook=False)
-net.from_nx(G)
-
-# Configuração das arestas para mostrar a ação quando o mouse passa por cima
-for edge in net.edges:
-    edge['title'] = edge['action']
-    edge['arrows'] = 'to' # Adicionando seta nas arestas
-
-# Exibindo o grafo no Streamlit
-with st.beta_container():
-    st.write(net)
-
+# Exibe o arquivo html
+st.markdown(html_file)
