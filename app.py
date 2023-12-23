29a1064d9cf4192df65c04d5b0eebbbee2a30426
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2023-12-23 13:17:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 # https://discuss.streamlit.io/t/unable-to-load-html-file-in-streamlit-app/40186/2
 
 import streamlit as st
-from pyvis.network import Network
+import pyvis.network as Network
 import networkx as nx
-import os
 
 # Criação do grafo direcionado
 G = nx.DiGraph()
@@ -22,7 +21,7 @@ G.add_edge("Maria", "tia", action="ficou para")
 G.add_edge("Joaquim", "suicídio", action="cometeu")
 G.add_edge("Lili", "J. Pinto Fernandes", action="casou com")
 G.add_edge("J. Pinto Fernandes", "história", action="que não tinha entrado na")
-G.add_edge("Quadrilha", "Drummond", action="autor")
+G.add_edge("Quadrilha","Drummond", action="autor")
 
 # Criação do grafo interativo
 net = Network(notebook=False)
@@ -31,15 +30,9 @@ net.from_nx(G)
 # Configuração das arestas para mostrar a ação quando o mouse passa por cima
 for edge in net.edges:
     edge['title'] = edge['action']
-    edge['arrows'] = 'to'  # Adicionando seta nas arestas
+    edge['arrows'] = 'to' # Adicionando seta nas arestas
 
-# Salvar o grafo como um arquivo HTML temporário
-temp_html_file = "temp_graph.html"
-net.show(temp_html_file)
+# Exibindo o grafo no Streamlit
+with st.beta_container():
+    st.write(net)
 
-# Exibir o grafo HTML no Streamlit
-with open(temp_html_file, "r", encoding="utf-8") as f:
-    st.components.v1.html(f.read(), width=800, height=600)
-
-# Remover o arquivo temporário
-os.remove(temp_html_file)
