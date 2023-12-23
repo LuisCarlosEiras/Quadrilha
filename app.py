bf9b9915346324ce685fdb9362879ee2419e1ad4
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2023-12-23 13:27:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
-# Read the grafo.html file
-with open("grafo.html", "r") as f:
-  html_data = f.read()
+# Read the HTML file
+with open("example_page.html", "r") as f:
+    html_data = f.read()
 
-# Display the grafo.html using st.markdown
-st.markdown(html_data, unsafe_allow_html=True)
+
