fbe83216156a8e28fa30101557ab3fc4ccfa0a02
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2023-12-23 13:30:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import streamlit as st
 with open("grafo.html", "r") as f:
     html_data = f.read()
     
+st.code('''st.markdown(f'<iframe srcdoc="{html_data}" width="1000" height="600"></iframe>', unsafe_allow_html=True)''', language='python')
+st.markdown(f'<iframe srcdoc="{html_data}" width="1000" height="600"></iframe>', unsafe_allow_html=True)
+
 st.code('''st.components.v1.html(html_data)''', language='python')
 st.components.v1.html(html_data)
 
