e1f5a66237c781d6f7e1c95da1f660be4a905cb2
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2023-12-23 13:10:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
 # https://discuss.streamlit.io/t/unable-to-load-html-file-in-streamlit-app/40186/2
 
 import streamlit as st
+from pyvis.network import Network
+import networkx as nx
 
-# Read the HTML file
-with open("grafo.html", "r") as f:
-    html_data = f.read()
+# Criação do grafo direcionado
+G = nx.DiGraph()
 
-st.header("Quadrilha")
+# Adicionando as arestas ao grafo
+G.add_edge("João", "Teresa", action="amava")
+G.add_edge("Teresa", "Raimundo", action="amava")
+G.add_edge("Raimundo", "Maria", action="amava")
+G.add_edge("Maria", "Joaquim", action="amava")
+G.add_edge("Joaquim", "Lili", action="amava")
+G.add_edge("Lili", "ninguém", action="amava")
+G.add_edge("João", "Estados Unidos", action="foi pra")
+G.add_edge("Teresa", "convento", action="foi para")
+G.add_edge("Raimundo", "desastre", action="morreu de")
+G.add_edge("Maria", "tia", action="ficou para")
+G.add_edge("Joaquim", "suicídio", action="cometeu")
+G.add_edge("Lili", "J. Pinto Fernandes", action="casou com")
+G.add_edge("J. Pinto Fernandes", "história", action="que não tinha entrado na")
+G.add_edge("Quadrilha", "Drummond", action="autor")
 
-st.markdown(f'<iframe srcdoc="{html_data}" width="1000" height="600"></iframe>', unsafe_allow_html=True)
+# Criação do grafo interativo
+net = Network(notebook=False)
+net.from_nx(G)
 
-# st.subheader('Using components.v1.html')
-st.code('''st.components.v1.html(html_data)''', language='python')
-st.components.v1.html(html_data)
+# Configuração das arestas para mostrar a ação quando o mouse passa por cima
+for edge in net.edges:
+    edge['title'] = edge['action']
+    edge['arrows'] = 'to'  # Adicionando seta nas arestas
+
+# Mostrando o grafo no Streamlit
+st.write(net.show("inline.html"))
 
 
