1804f41787839c987a75d3f4d54a5b1690e3105a
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2023-12-23 13:58:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 st.header("Quadrilha, Drummond")
-st.subheader('Using st.markdown with iframe')
+st.subheader('Com o mouse sobre a seta, aparece o verbo')
+st.subheader('Apertando o botão do mouse sobre a aresta(círculo), você pode movimentar o grafo') 
 # Read the HTML file
 with open("grafo.html", "r") as f:
     html_data = f.read()
