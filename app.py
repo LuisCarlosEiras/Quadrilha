5b23421a0079ab01026fa6e6b2e4f2674a79d54f
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2023-12-23 13:34:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import streamlit as st
 # Read the HTML file
 with open("grafo.html", "r") as f:
     html_data = f.read()
+
+st.header("Quadrilha")
     
 # st.code('''st.markdown(f'<iframe srcdoc="{html_data}" width="1000" height="600"></iframe>', unsafe_allow_html=True)''', language='python')
 # st.markdown(f'<iframe srcdoc="{html_data}" width="1000" height="600"></iframe>', unsafe_allow_html=True)
