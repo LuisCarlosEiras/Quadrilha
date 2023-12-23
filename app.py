83a210cf5fe85173b69222e9b2db6026c3026d51
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2023-12-23 13:28:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ import streamlit as st
 # Read the HTML file
 with open("example_page.html", "r") as f:
     html_data = f.read()
+    
+st.code('''st.components.v1.html(html_data)''', language='python')
+st.components.v1.html(html_data)
 
 
