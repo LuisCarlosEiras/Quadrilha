22f7935641693d9f59cdbdb380651dec3507f65d
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2023-12-23 12:03:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,25 @@
-from pyvis.network import Network
-import networkx as nx
+import streamlit as st
 
-# Criação do grafo direcionado
-G = nx.DiGraph()
+# Read the HTML file
+with open("grafo.html", "r") as f:
+    html_data = f.read()
 
-# Adicionando as arestas ao grafo
-G.add_edge("João", "Teresa", action="amava")
-G.add_edge("Teresa", "Raimundo", action="amava")
-G.add_edge("Raimundo", "Maria", action="amava")
-G.add_edge("Maria", "Joaquim", action="amava")
-G.add_edge("Joaquim", "Lili", action="amava")
-G.add_edge("Lili", "ninguém", action="amava")
-G.add_edge("João", "Estados Unidos", action="foi pra")
-G.add_edge("Teresa", "convento", action="foi para")
-G.add_edge("Raimundo", "desastre", action="morreu de")
-G.add_edge("Maria", "tia", action="ficou para")
-G.add_edge("Joaquim", "suicídio", action="cometeu")
-G.add_edge("Lili", "J. Pinto Fernandes", action="casou com")
-G.add_edge("J. Pinto Fernandes", "história", action="que não tinha entrado na")
-G.add_edge("Quadrilha","Drummond", action="autor")
+st.header("Deepchecks")
 
-# Criação do grafo interativo
-net = Network(notebook=True)
-net.from_nx(G)
+st.write('---')
 
-# Configuração das arestas para mostrar a ação quando o mouse passa por cima
-for edge in net.edges:
-    edge['title'] = edge['action']
-    edge['arrows'] = 'to' # Adicionando seta nas arestas
-# cdn_resources='in_line'
-# Mostrando o grafo
-net.show("grafo.html")
+st.subheader('Using st.markdown')
+st.code('''st.markdown(html_data, unsafe_allow_html=True)''', language='python')
+st.markdown(html_data, unsafe_allow_html=True)
+
+st.write('---')
+
+st.subheader('Using st.markdown with iframe')
+st.code('''st.markdown(f'<iframe srcdoc="{html_data}" width="1000" height="600"></iframe>', unsafe_allow_html=True)''', language='python')
+st.markdown(f'<iframe srcdoc="{html_data}" width="1000" height="600"></iframe>', unsafe_allow_html=True)
+
+st.write('---')
+
+st.subheader('Using components.v1.html')
+st.code('''st.components.v1.html(html_data)''', language='python')
+st.components.v1.html(html_data)
